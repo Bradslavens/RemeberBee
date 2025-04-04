5e668af8ca907474ed8464a5f31455dce07d1cdc
Brad Slavens <bradslavens@gmail.com>
2025-04-04 08:27:50 -0700

    signal list multidimensional, drop down to select line

--- signals.js
+++ signals.js
@@ -2,9 +2,16 @@
 
 // Ordered collection of signals
 const signals = [
-  "S154", "S16RA", "S226", "S296", "S354", "S406", "S24RA", "S32RA", "S592", "S662",
-  "S40RA", "S816", "S44RA", "S916", "S984", "S54RA", "S1172", "S58RA", "S1332", "S62RA", "S1402", "S98RA"
-  ];
+  ["Blue Line South East"["S154", "S16RA", "S226", "S296", "S354", "S406", "S24RA", "S32RA", "S592", "S662",
+    "S40RA", "S816", "S44RA", "S916", "S984", "S54RA", "S1172", "S58RA", "S1332", "S62RA", "S1402", "S98RA"]],
+  ["Blue Line South West"],  
+  ["Blue Line North East"],
+  ["Blue Line North West"],
+  ["Orange Line East"],
+  ["Orange Line West"],
+  ["Green Line East"],
+  ["Green Line West"]
+];
   
   // Export the signals array
   export default signals;
